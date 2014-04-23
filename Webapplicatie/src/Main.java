fc9809be0da8af57830484bb40e9e1045bdf8c7e
nielshoiting27 <niels.hoiting@gmail.com>
2014-04-23 10:49:23 +0200

    fix

--- Webapplicatie/src/Main.java
+++ Webapplicatie/src/Main.java
@@ -2,12 +2,8 @@
 public class Main {
 	public static void main(String[] args){
 		System.out.println("eerste regel op regel 4");
-<<<<<<< HEAD
 		//HALLO
 		//tot
-		
-=======
 		System.out.println("tweede regel op regel 5");
->>>>>>> branch 'master' of https://github.com/autototaaldienst/applicatie.git
 	}
 }
